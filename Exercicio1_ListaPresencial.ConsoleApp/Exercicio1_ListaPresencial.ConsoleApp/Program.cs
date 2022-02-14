8156cb5974d734a6cbba57bb441f257d6e0b9556
Pedro Nunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-14 12:14:57 -0300

    Add files via upload

---
+++ Exercicio1_ListaPresencial.ConsoleApp/Exercicio1_ListaPresencial.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio1_ListaPresencial.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a largura do retângulo: ");
+            int largura = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite a altura do retângulo: ");
+            int altura = Convert.ToInt32(Console.ReadLine());
+
+            String asterisco = "";
+            for (int i = 0; i < altura; i++)
+            {
+                for (int j = 0; j < largura; j++)
+                {
+                    asterisco = asterisco + "*";
+                }
+                asterisco += "\n";
+            }
+            Console.WriteLine(asterisco);
+        }
+    }     
+}
+    
+
