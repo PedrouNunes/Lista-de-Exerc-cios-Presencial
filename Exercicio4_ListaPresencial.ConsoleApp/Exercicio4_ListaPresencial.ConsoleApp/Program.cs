cdfb3da322f27197ab4daf28942b3724a1d47051
Pedro Nunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-14 12:16:57 -0300

    Add files via upload

---
+++ Exercicio4_ListaPresencial.ConsoleApp/Exercicio4_ListaPresencial.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercicio4_ListaPresencial.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = new int[5];
+            int contNegativos = 0, contPositivos = 0;
+            double media = 0, media2;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Digite o valor " + i + ": ");
+                valores[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (valores[i] < 0)
+                {
+                    contNegativos++;
+                }
+                else
+                {
+                    contPositivos++;
+                }
+
+                media = media + valores[i];
+            }
+            media2 = media / 5;
+
+            Console.WriteLine("A Média Aritmética é: " + media2);
+            Console.WriteLine("A quantidade de valores positivos é: " + contPositivos);
+            Console.WriteLine("A quantidade de valores negativos é: " + contNegativos);
+
+        }
+    }
+}
