c7ea99e79903ac91198a9b3faad463359245fdad
Pedro Nunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-14 12:16:20 -0300

    Add files via upload

---
+++ Exercicio3_ListaPresencial.ConsoleApp/Exercicio3_ListaPresencial.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio3_ListaPresencial.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a hora: ");
+            int horaAtual = Convert.ToInt32(Console.ReadLine());
+
+            if (horaAtual > 5 && horaAtual < 12)
+            {
+                Console.WriteLine("Bom dia.");
+            }
+            else
+            {
+                if (horaAtual >= 12 && horaAtual < 18)
+                {
+                    Console.WriteLine("Boa tarde.");
+                }
+                else
+                {                 
+                        Console.WriteLine("Boa noite.");                
+                }
+            }
+        }
+    }
+}
